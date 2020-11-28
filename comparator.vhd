70e02631924ebc790bf296ae5b3bce5d83badff3
Josh Boudreau <joshuaboud@hotmail.com>
2020-11-28 16:03:34 -0400

    create comparator

--- comparator.vhd
+++ comparator.vhd
@@ -1,1 +1,24 @@
+-- comparator.vhd
+-- Authour: Josh Boudreau B00819096
+-- compare two values
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use ieee.std_logic_unsigned.all;
+
+entity comparator is
+    generic(
+        DEPTH : integer := 3 -- input width
+    );
+    Port (
+        a : in STD_LOGIC_VECTOR (DEPTH-1 downto 0);
+        b : in STD_LOGIC_VECTOR (DEPTH-1 downto 0);
+        result : out STD_LOGIC
+    );
+end comparator;
+
+architecture Behavioral of comparator is
+begin
+    result <= '1' when (a = b) else '0';
+end Behavioral;
  
--- test_benches/comparator_TB.vhd
+++ test_benches/comparator_TB.vhd
@@ -1,1 +1,39 @@
- 
+-- adder_TB.vhd
+-- Authour: Josh Boudreau B00819096
+-- adder testbench
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use ieee.std_logic_unsigned.all;
+
+entity comparator_TB is
+end comparator_TB;
+
+architecture Behavioral of comparator_TB is
+    component comparator is
+        generic(
+            DEPTH : integer := 3 -- input width
+        );
+        Port (
+            a : in STD_LOGIC_VECTOR (DEPTH-1 downto 0);
+            b : in STD_LOGIC_VECTOR (DEPTH-1 downto 0);
+            result : out STD_LOGIC
+        );
+    end component;
+    signal a : STD_LOGIC_VECTOR (2 downto 0) := (others => '0');
+    signal b : STD_LOGIC_VECTOR (2 downto 0) := (others => '0');
+    signal result : STD_LOGIC;
+begin
+    add : comparator port map (a => a, b => b, result => result);
+    test_comparator: process
+    begin
+        for i in 0 to 7 loop
+            for j in 0 to 7 loop
+                wait for 20 ns;
+                a <= a + '1';
+            end loop;
+            b <= b + '1';
+        end loop;
+    end process;
+end Behavioral;
+
